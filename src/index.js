7ef49204971c63e8b571b13ca867cdd8952ebc5d
ArtAlexArt <alexander.artyomchenko@gmail.com>
2022-01-12 16:12:49 +0200

    feat: create fun reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  if (n < 0) n = -n;
+  var s = n.toString();
+  let len = s.length;
+  const lenHalf = (len / 2) >> 0;
+  len -= 1;
+
+  var s1 = "";
+  var res = s.split("");
+  for (let i = 0; i < lenHalf; i++) {
+    res[i] = s[len - i];
+    res[len - i] = s[i];
+  }
+  return Number(res.join(""));
+};
